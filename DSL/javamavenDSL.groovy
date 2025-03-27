1e934e61662f8db5e71e5f7b01317dcf2117693f
Ale Albaladejo <159116346+alealbaladejo@users.noreply.github.com>
2025-03-27 09:06:42 +0100

    Update javamavenDSL.groovy

--- DSL/javamavenDSL.groovy
+++ DSL/javamavenDSL.groovy
@@ -1,9 +1,9 @@
 job('Java Maven App DSL') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/alealbaladejo/simple-java-maven-app.git', 'master') { node ->
+            node / gitConfigName('alealbaladejo')
+            node / gitConfigEmail('alealbaladejo29s@gmail.com')
         }
     }
     steps {
